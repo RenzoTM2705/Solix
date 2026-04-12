b818c2867a5363a18cbc138311cf5e470f8cba18
RenzoTM2705 <renzotaypicahuana27@gmail.com>
2026-04-11 21:48:31 -0500

    vista gastos programados agregada

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { Login } from './screens/Login';
 import { ConfigInicial } from './screens/ConfigInicial';
 import { Dashboard } from './screens/Dashboard';
 import { Registros } from './screens/Registros';
+import { GastosProgramados } from './screens/GastosProgramados';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/config-inicial" element={<ConfigInicial />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/registros" element={<Registros />} />
+        <Route path="/gastos-programados" element={<GastosProgramados />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
---
+++ src/screens/GastosProgramados/index.ts
@@ -0,0 +1,1 @@
+export {GastosProgramados} from './GastosProgramados';
